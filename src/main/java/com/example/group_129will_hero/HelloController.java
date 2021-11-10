67241a5357ee09e1ed2609de8235e94bf9519cd3
mag004 <manasagarwal005@gmail.com>
2021-11-10 13:13:58 +0530

    JavaFX SetUp

---
+++ src/main/java/com/example/group_129will_hero/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.group_129will_hero;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.group_129will_hero {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.group_129will_hero to javafx.fxml;
+    exports com.example.group_129will_hero;
+}
